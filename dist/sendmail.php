89652b9a7ae3c34ba578db1f02e2098ea7b37269
MikhailTravin <78166836+MikhailTravin@users.noreply.github.com>
2026-04-25 17:46:17 +0300

    Add files via upload

--- dist/sendmail.php
+++ dist/sendmail.php
@@ -3,7 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Проверка путей
 $phpmailer_path = __DIR__ . '/PHPMailer/';
 
 if (!file_exists($phpmailer_path . 'src/Exception.php')) {
@@ -38,13 +37,13 @@ $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
 $mail->IsHTML(false);
 
-$mail->setFrom('elogica@inbox.ru', 'EcoOcean');
-$mail->addAddress('elogica@inbox.ru');
+$mail->setFrom('mikhail.travin@gmail.com', 'EcoOcean');
+$mail->addAddress('mikhail.travin@gmail.com');
 
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'elogica@inbox.ru';
+$mail->Username = 'mikhail.travin@gmail.com';
 $mail->Password = 'pogv nnfc uvru xrll';
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
@@ -74,9 +73,7 @@ if (isset($_POST['form']) && is_array($_POST['form'])) {
 	if (isset($_POST['form'][2]) && !empty($_POST['form'][2])) {
 		$body .= 'Phone: ' . $_POST['form'][2] . "\n";
 	}
-	if (isset($_POST['form'][3]) && !empty($_POST['form'][3])) {
-		$body .= 'Subject: ' . $_POST['form'][3] . "\n";
-	}
+	// Убрал проверку на form[3] так как его нет в вашей форме
 }
 
 $mail->Body = $body;
--- dist/subscribe.php
+++ dist/subscribe.php
@@ -1,41 +1,94 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Проверка путей
+$phpmailer_path = __DIR__ . '/PHPMailer/';
+
+if (!file_exists($phpmailer_path . 'src/Exception.php')) {
+	$response = ['message' => 'Ошибка: не найден Exception.php в ' . $phpmailer_path . 'src/'];
+	header('Content-type: application/json');
+	echo json_encode($response);
+	exit;
+}
+
+if (!file_exists($phpmailer_path . 'src/PHPMailer.php')) {
+	$response = ['message' => 'Ошибка: не найден PHPMailer.php в ' . $phpmailer_path . 'src/'];
+	header('Content-type: application/json');
+	echo json_encode($response);
+	exit;
+}
+
+if (!file_exists($phpmailer_path . 'src/SMTP.php')) {
+	$response = ['message' => 'Ошибка: не найден SMTP.php в ' . $phpmailer_path . 'src/'];
+	header('Content-type: application/json');
+	echo json_encode($response);
+	exit;
+}
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/PHPMailer/src/Exception.php';
-require __DIR__ . '/PHPMailer/src/PHPMailer.php';
-require __DIR__ . '/PHPMailer/src/SMTP.php';
+require $phpmailer_path . 'src/Exception.php';
+require $phpmailer_path . 'src/PHPMailer.php';
+require $phpmailer_path . 'src/SMTP.php';
 
 $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
 $mail->IsHTML(false);
 
-$mail->setFrom('elogica@inbox.ru', 'EcoOcean');
-$mail->addAddress('elogica@inbox.ru');
+$mail->setFrom('mikhail.travin@gmail.com', 'EcoOcean');
+$mail->addAddress('mikhail.travin@gmail.com');
 
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'elogica@inbox.ru';
+$mail->Username = 'mikhail.travin@gmail.com';
 $mail->Password = 'pogv nnfc uvru xrll';
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
+$mail->Debugoutput = 'html';
 
-$mail->Subject = 'New subscription on the site';
+$buttonSubject = isset($_POST['button_subject']) ? trim($_POST['button_subject']) : '';
 
-$email = isset($_POST['email']) ? trim($_POST['email']) : '';
+if (!empty($buttonSubject)) {
+	$mail->Subject = $buttonSubject;
+} else {
+	$mail->Subject = 'Новая заявка с сайта';
+}
 
-$body = "New subscription!\n\n";
-$body .= "Email: " . $email . "\n";
-$body .= "Date: " . date('d.m.Y H:i:s') . "\n";
+$body = '';
+
+if (isset($_POST['form']) && is_array($_POST['form'])) {
+	if (!empty($buttonSubject)) {
+		$body .= $buttonSubject . "\n\n";
+	}
+
+	if (isset($_POST['form'][0]) && !empty($_POST['form'][0])) {
+		$body .= 'Name: ' . $_POST['form'][0] . "\n";
+	}
+	if (isset($_POST['form'][1]) && !empty($_POST['form'][1])) {
+		$body .= 'Email: ' . $_POST['form'][1] . "\n";
+	}
+	if (isset($_POST['form'][2]) && !empty($_POST['form'][2])) {
+		$body .= 'Phone: ' . $_POST['form'][2] . "\n";
+	}
+	if (isset($_POST['form'][3]) && !empty($_POST['form'][3])) {
+		$body .= 'Subject: ' . $_POST['form'][3] . "\n";
+	}
+}
 
 $mail->Body = $body;
 
-if (!$mail->send()) {
-    $message = 'Error';
-} else {
-    $message = 'Subscribed successfully!';
+try {
+	if (!$mail->send()) {
+		$message = 'Ошибка: ' . $mail->ErrorInfo;
+	} else {
+		$message = 'Данные отправлены!';
+	}
+} catch (Exception $e) {
+	$message = 'Ошибка: ' . $e->getMessage();
 }
 
 $response = ['message' => $message];
